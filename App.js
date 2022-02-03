bc3e35fedc27bcd031ef0afe493b405c7d031a47
Guilherme Stetes <guilherme@dobesone.com.br>
2022-02-03 18:03:29 -0300

    03022022

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    <View>
-      <Text>React Native</Text>
-    </View>
-  );
-};
+  return <Home />;
+}
