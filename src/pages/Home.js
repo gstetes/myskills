0b0817255eb106f6600ade62009552d2bc810d7d
Guilherme Stetes <guilherme@dobesone.com.br>
2022-02-04 18:01:27 -0300

    04022022

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   Platform,
-  TouchableOpacity,
+  FlatList,
 } from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
 
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
-    setNewSkill('');
-  };
+    setMySkills((oldState) => [...oldState, newSkill]);
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours() - 3;
+
+    if (currentHour < 12) {
+      setGretting('Good morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting('Good afternoon');
+    } else {
+      setGretting('Good night');
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Guilherme 👋</Text>
+
+      <Text style={styles.greetings}>{gretting}</Text>
 
       <TextInput
         style={styles.input}
@@ -28,20 +44,16 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity style={styles.button} activeOpacity={0.7} onPress={handleAddNewSkill}>
-        <Text style={styles.buttonText}>Add</Text>
-      </TouchableOpacity>
+      <Button onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
 
-      {mySkills.map(skill => (
-        <TouchableOpacity key={skill} style={styles.buttonSkill}>
-          <Text style={styles.textSkill}>
-            {skill}
-          </Text>
-        </TouchableOpacity>
-      ))}
-
+      <FlatList
+        data={mySkills}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <SkillCard skill={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
@@ -66,28 +78,7 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderRadius: 7,
   },
-  button: {
-    backgroundColor: '#a370f7',
-    padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  buttonText: {
+  greetings: {
     color: '#FFF',
-    fontSize: 17,
-    fontWeight: 'bold',
   },
-  buttonSkill: {
-    backgroundColor: '#1F1E25',
-    padding: 15,
-    borderRadius: 50,
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  textSkills: {
-    color: '#FFF',
-    fontSize: 22,
-    fontWeight: 'bold'
-  }
 });
